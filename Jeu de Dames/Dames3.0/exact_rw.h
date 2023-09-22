d221f824184e9dd01a780a1a7257cb02227079b8
Alex <114581163+BeteMachine@users.noreply.github.com>
2023-09-22 23:39:49 +0200

    Delete Jeu de Dames/Dames3.0 directory

--- Jeu de Dames/Dames3.0/exact_rw.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXACT_RW_H
-#define EXACT_RW_H
-#include <unistd.h> 
-ssize_t exact_write(int fd, void *buf, size_t nbytes);
-ssize_t exact_read(int fd, void *buf, size_t nbytes);
-
-
-
-#endif
